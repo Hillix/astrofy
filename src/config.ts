b636cfe1575f0c0822e8589707d88ac46bc72f6c
Nwobani Hillary <114597659+Hillix@users.noreply.github.com>
2024-11-26 12:30:32 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Nwobani | Digital Marketing Specialist';
-export const SITE_DESCRIPTION = 'A digital marketer with specialization in creating and executing online marketing strategies to drive brand growth and engagement. With expertise in SEO, social media marketing, content creation, and paid advertising, you leverage data-driven insights to optimize campaigns and maximize ROI. Your ability to adapt to evolving digital trends allows you to effectively reach and connect with target audiences, ensuring impactful online presence and business success.';
+export const SITE_DESCRIPTION = 'A digital marketer with specialization in creating and executing online marketing strategies to drive brand growth and engagement. With expertise in SEO, social media marketing, content creation, and paid advertising, I leverage on data-driven insights to optimize campaigns and maximize ROI. My ability to adapt to evolving digital trends allows me to effectively reach and connect with targeted audiences, ensuring impactful online presence and business success.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
