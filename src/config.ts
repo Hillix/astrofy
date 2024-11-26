e322bc3c35149ea285959174bdf69231c340b9a0
Nwobani Hillary <114597659+Hillix@users.noreply.github.com>
2024-11-26 12:24:34 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Nwobani | Digital Marketing Specialist';
+export const SITE_DESCRIPTION = 'A digital marketer with specialization in creating and executing online marketing strategies to drive brand growth and engagement. With expertise in SEO, social media marketing, content creation, and paid advertising, you leverage data-driven insights to optimize campaigns and maximize ROI. Your ability to adapt to evolving digital trends allows you to effectively reach and connect with target audiences, ensuring impactful online presence and business success.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
